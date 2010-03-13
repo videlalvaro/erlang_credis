b3fd376cc0fe182bb4a7cba69ce398061b13b7ae
Alvaro Videla <videlalvaro@gmail.com>
2010-03-13 18:07:16 +0800

    new test/1 function
    function directly imported from riak_redis_backend
    to test integration with riak

--- src/rd_test.erl
+++ src/rd_test.erl
@@ -1,6 +1,6 @@
 -module(rd_test).
 
--export([test/0]).
+-export([test/0, test/1]).
 
 test() ->
   redis_drv:start_link(),
@@ -11,3 +11,9 @@ test() ->
   
 log(Msg) ->
   io:format("Log: ~p~n", [Msg]).
+
+test(riak) ->
+  riak_test_util:standard_backend_test(riak_redis_backend, []),
+  fprof:apply(riak_test_util,standard_backend_test, [riak_redis_backend, []]),
+  fprof:profile(),
+  fprof:analyse([no_callers, no_details, totals]).
